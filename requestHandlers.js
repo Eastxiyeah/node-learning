359876e34b8a8fede8b1ceba6940f3588f4103fe
xiedong <eastxiyeah@dingtalk.com>
2024-03-05 16:57:24 +0800

    区分路由以及调用对应请求处理程序

---
+++ requestHandlers.js
@@ -0,0 +1,7 @@
+exports.start = () => {
+  console.log("Request handler 'start' was called.");
+};
+
+exports.upload = () => {
+  console.log("Request handler 'upload' was called.");
+};
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const http = require("http");
+const http = require('http');
+const url = require('url')
 
-http
-  .createServer((req, res) => {
-    console.log("Request received");
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Good job");
-    res.end();
-  })
-  .listen(8888);
+const start = (route, handle) => {
+  http
+    .createServer((req, res) => {
+      const pathname = url.parse(req.url).pathname
+      console.log(`Request for ${pathname} received`);
 
-console.log("Server has started");
+      route(handle, pathname)
+
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write('Good job');
+      res.end();
+    })
+    .listen(8888);
+
+  console.log('Server has started');
+};
+
+exports.start = start;
